a5149f4e179e081b3f483f1d92835a92710957a2
Aleksandar Aleksandrov <aleks.aleksandrov@yahoo.com>
2016-04-11 14:47:22 +0300

    Added files via upload

---
+++ C# Programs/SimpleConditions/Numbers0to9asText/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> array = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        int input = int.Parse(Console.ReadLine());
+
+        if (input <= 9)
+        {
+            Console.WriteLine(array[input-1]);
+        }
+        else
+        {
+            Console.WriteLine("number too big");
+        }
+    }
+}
